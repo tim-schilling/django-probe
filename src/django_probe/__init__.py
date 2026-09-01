0b680b2ce4c3d0dbeaa07bf79e5fe5ee37a671ca
Tim Schilling <schillingt@better-simple.com>
2026-09-01 16:03:49 -0500

    Hook up the CI and release mechanisms.

--- src/django_probe/__init__.py
+++ src/django_probe/__init__.py
@@ -2,6 +2,10 @@
 
 from __future__ import annotations
 
+import os
+
 __all__ = ["__version__"]
 
-__version__ = "0.1.0"
+# Suffixed with DJANGO_PROBE_VERSION_DEV so CI can build throwaway dev
+# distributions to test the release process. See .github/workflows/test_release.yml
+__version__ = "0.1.0" + os.environ.get("DJANGO_PROBE_VERSION_DEV", "")
